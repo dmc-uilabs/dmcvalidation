8962e75b1d68bc4feec52afe76af51cb092b39b1
alex maties <alex.maties@uilabs.org>
2016-07-14 09:15:17 -0500

    new packages and tags test

--- routes/routes.js
+++ routes/routes.js
@@ -28,22 +28,36 @@ app.post("/", function(req, res) {
               console.log('the current working dir is : ',data)
 
 
-              var validated = {
-                     "id": req.body.id,
-                     "url": req.body.url,
-                     "table": req.body.table,
-                     "folder": "Profile",
-                     "resourceType": "ProfilePicture",
-                     "userEPPN": req.body.userEPPN,
-                     "verified": "garbage",
-                     "scanLog":data.substring(0, (data.indexOf(":")+4))
-                   }
-
-                          proxy.web(req, res.send(validated), {
-                            target: 'http://localhost:8080'
-                          });
+              var validated={
+                "name": "testtag555",
+                "projectId": "6"
+              }
 
 
+              // var validated = {
+              //        "id": req.body.id,
+              //        "url": req.body.url,
+              //        "table": req.body.table,
+              //        "folder": "Profile",
+              //        "resourceType": "ProfilePicture",
+              //        "userEPPN": req.body.userEPPN,
+              //        "verified": "garbage",
+              //        "scanLog":data.substring(0, (data.indexOf(":")+4))
+              //      }
+
+                          // proxy.web(req, res.send(validated), {
+                          //   target: 'http://localhost:8080/projects_tags/'
+                          // });
+var needle = require('needle');
+var options = {
+  headers: { "Content-Type": "application/json","AJP_eppn": "fforgeadmin" }
+}
+
+needle.post('http://localhost:8080/projects_tags/', JSON.stringify(validated), options, function(err, resp) {
+  // you can pass params as a string or as an object.
+  console.log("guess whose back ");
+  console.log(resp.body)
+});
 
 
           }
@@ -54,7 +68,7 @@ app.post("/", function(req, res) {
 
   });
 
-
+res.send('hello world');
 });
 
 
