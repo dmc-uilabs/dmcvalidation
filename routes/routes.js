66936dc210a4363c26ed05d303e240fe72343f13
alex maties <alex.maties@uilabs.org>
2017-05-10 00:26:32 -0500

    validation machine with quarantine

--- routes/routes.js
+++ routes/routes.js
@@ -1,102 +1,166 @@
 var appRouter = function(app) {
-var proxy = httpProxy.createProxyServer();
+  var proxy = httpProxy.createProxyServer();
 
-app.get('/', function (req, res) {
-  res.send('validation online')
-})
+  app.get('/', function(req, res) {
+    res.send('validation online')
+  })
 
-app.post("/", function(req, res) {
+  app.post("/", function(req, res) {
 
-  var fs = require('fs'),
+    fs = require('fs'),
       request = require('request');
 
-  var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
+    console.log('reached post')
 
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-  };
-
-  console.log("request",req);
-
-  download(req.body.url, 'File', function(){
-    console.log('done');
+    function getDocument(uri, filename, callback) {
 
 
+      console.log("got url ", uri)
+      var credentials = {
 
-  //Sha256 Calculation
-
-  var crypto = require('crypto');
-  var fs = require('fs');
-  var algo='sha256';
-  var shasum =crypto.createHash(algo);
-  var sha ='';
-  var file = 'File';
-  var s = fs.ReadStream(file);
-  s.on('data', function(sha) { shasum.update(sha); });
-  s.on('end', function() {
-    sha = shasum.digest('hex');
-    console.log(sha);
-  });
+        accessKeyId: process.env['VAL_S3_KEY'],
+        secretAccessKey: process.env['VAL_S3_SECRET'],
+        region: 'us-east-1'
+      };
 
 
+      var AWS = require('aws-sdk')
+      var s3 = new AWS.S3(credentials)
 
 
- var cmd=require('node-cmd');
-  cmd.get(
+      var params = {
+        Bucket: "dmcuptemp",
+        Key: uri.substring(uri.indexOf("m/") + 2)
+      };
+      console.log(uri)
+      console.log(params);
+      var file = require('fs').createWriteStream(filename);
 
-          // 'sha256 File'
+      s3.getObject(params).on('httpData', function(chunk) {
+        file.write(chunk);
+      }).on('httpDone', function() {
 
-          'clamscan File',
-          function(data){
-              console.log('the current working dir is : ',data);
+        file.end();
+        console.log("got all the file");
 
-              //Default set to false
-              var verified = false;
-              var scanResult = data.substring(0, (data.indexOf(":")+4))
+        callback(filename);
+      }).send();
 
-              if(scanResult =='File: OK')
-                verified = true;
+    }
+
+    getDocument(req.body.url, "file", afterDownload);
 
 
-              //Return object
-               var validated = {
-                      "id": req.body.id,
-                      "url": req.body.url,
-                      "table": req.body.table,
-                      "folder": req.body.folder,
-                      "resourceType": req.body.resourceType,
-                      "userEPPN": req.body.userEPPN,
-                      "urlColumn": req.body.urlColumn,
-                      "idColumn": req.body.idColumn,
-                      "verified": verified,
-                      "scanLog":scanResult,
-		                  "restIP":req.body.restIP,
-                      "sha256":sha
-                    }
 
-                    console.log(validated);
+    function afterDownload(filename) {
+      console.log("inside the after the dowload ");
 
-var needle = require('needle');
-var options = {
-  headers: { "Content-Type": "application/json","AJP_eppn": req.body.userEPPN }
-}
+      var cmd = require('node-cmd');
+      cmd.get('clamscan file', function(data) {
+        console.log('the current working dir is : ', data);
 
-//Call back to the REST server
-needle.post("http:/\/"+validated.restIP+':8080/rest/verify/', JSON.stringify(validated), options, function(err, resp) {
-  // you can pass params as a string or as an object.
-  console.log("guess whose back ");
+        //Default set to false
+        var verified = false;
+        var scanResult = data.substring(0, (data.indexOf(":") + 4))
+        // the scan results are true
+        if (scanResult == (filename + ': OK')) {
+          verified = true;
 
-});
+          // calculate the sha
+          var crypto = require('crypto');
+          var fs = require('fs');
+          var algo = 'sha256';
+          var shasum = crypto.createHash(algo);
+          var sha = '';
+          fs.ReadStream(filename).on('data', function(sha) {
+            shasum.update(sha);
+          }).on('end', function() {
+            sha = shasum.digest('hex');
+            console.log(sha);
 
+
+
+
+            //Return object
+            var validated = {
+              "id": req.body.id,
+              "url": req.body.url,
+              "table": req.body.table,
+              "folder": req.body.folder,
+              "resourceType": req.body.resourceType,
+              "userEPPN": req.body.userEPPN,
+              "urlColumn": req.body.urlColumn,
+              "idColumn": req.body.idColumn,
+              "verified": verified,
+              "scanLog": scanResult,
+              "restIP": req.body.restIP,
+              "sha256": sha
+            }
+
+            console.log(validated);
+
+            var needle = require('needle');
+            var options = {
+              headers: {
+                "Content-Type": "application/json",
+                "AJP_eppn": req.body.userEPPN
+              }
+            }
+
+            //Call back to the REST server
+            needle.post("http:/\/" + validated.restIP + ':8080/rest/verify/', JSON.stringify(validated), options, function(err, resp) {
+              // you can pass params as a string or as an object.
+              console.log("guess whose back ");
+
+            });
+
+
+
+
+
+          });
+
+        } else {
+          // move to quarantine folder
+          console.log("moving to quarantine")
+          var validated = {
+            "id": req.body.id,
+            "url": req.body.url,
+            "table": req.body.table,
+            "folder": req.body.folder,
+            "resourceType": req.body.resourceType,
+            "userEPPN": req.body.userEPPN,
+            "urlColumn": req.body.urlColumn,
+            "idColumn": req.body.idColumn,
+            "verified": false,
+            "scanLog": scanResult,
+            "restIP": req.body.restIP,
+            "sha256": "failed validation"
           }
-      );
-  });
 
-res.send('validation reached');
-});
+          console.log(validated);
+
+          var needle = require('needle');
+          var options = {
+            headers: {
+              "Content-Type": "application/json",
+              "AJP_eppn": req.body.userEPPN
+            }
+          }
+
+          //Call back to the REST server
+          needle.post("http:/\/" + validated.restIP + ':8080/rest/verify/', JSON.stringify(validated), options, function(err, resp) {
+            // you can pass params as a string or as an object.
+            console.log("guess whose back ");
+
+          });
+        }
+
+      });
+    }
+
+    res.send('validation reached');
+  });
 
 }
 module.exports = appRouter;
